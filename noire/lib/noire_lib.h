119ee62d0d322751a991bf665e979dac6c1be8c8
Dmitry Simonenko <pmwkaa@gmail.com>
2023-12-15 19:47:31 +0200

    lib: add module

---
+++ noire/lib/noire_lib.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//
+// noire
+//
+// time-series storage
+//
+
+#include "lib/rbtree.h"
+#include "lib/id_mgr.h"
+#include "lib/crc.h"
+#include "lib/compression.h"
+#include "lib/config.h"
+#include "lib/global.h"
