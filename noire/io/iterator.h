c5cdf6be7152ca94cf729817e59ce78993d00d78
Dmitry Simonenko <pmwkaa@gmail.com>
2023-12-15 19:52:05 +0200

    io: add iterator interface

---
+++ noire/io/iterator.h
@@ -0,0 +1,47 @@
+#pragma once
+
+//
+// noire
+//
+// time-series storage
+//
+
+typedef struct Iterator Iterator;
+
+typedef bool (*IteratorHas)(Iterator*);
+typedef Row* (*IteratorAt)(Iterator*);
+typedef void (*IteratorNext)(Iterator*);
+typedef void (*IteratorClose)(Iterator*);
+
+struct Iterator
+{
+	IteratorHas   has;
+	IteratorAt    at;
+	IteratorNext  next;
+	IteratorClose close;
+};
+
+static inline bool
+iterator_has(Iterator* self)
+{
+	return self->has(self);
+}
+
+static inline Row*
+iterator_at(Iterator* self)
+{
+	return self->at(self);
+}
+
+static inline void
+iterator_next(Iterator* self)
+{
+	self->next(self);
+}
+
+static inline void
+iterator_close(Iterator* self)
+{
+	if (self->close)
+		self->close(self);
+}
