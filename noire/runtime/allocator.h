ab62bf582560112b4ddb82a66c0e0da685201659
Dmitry Simonenko <pmwkaa@gmail.com>
2023-12-15 13:07:01 +0200

    runtime: add malloc/free functions

---
+++ noire/runtime/allocator.h
@@ -0,0 +1,31 @@
+#pragma once
+
+//
+// noire
+//
+// time-series storage
+//
+
+static inline void*
+nr_malloc(size_t size)
+{
+	auto ptr = malloc(size);
+	if (unlikely(ptr == NULL))
+		error_system();
+	return ptr;
+}
+
+static inline void*
+nr_realloc(void* pointer, size_t size)
+{
+	auto ptr = realloc(pointer, size);
+	if (unlikely(ptr == NULL))
+		error_system();
+	return ptr;
+}
+
+static inline void
+nr_free(void* pointer)
+{
+	free(pointer);
+}
