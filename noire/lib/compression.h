cd2523dea0de38abe88c49b0d167f16803b4b535
Dmitry Simonenko <pmwkaa@gmail.com>
2023-12-15 19:47:43 +0200

    lib: add compression wrapper

---
+++ noire/lib/compression.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//
+// noire
+//
+// time-series storage
+//
+
+enum
+{
+	COMPRESSION_OFF  = 0,
+	COMPRESSION_ZSTD = 1
+};
+
+int  compression_get(const char*, int);
+void compression_compress(Buf*, int, Buf*, Buf*);
+void compression_decompress(Buf*, int, int, char*, int);
+bool compression_is_set(int);
