595db18d9bfb1befbce8eb4c2f51e2ca9d0d941c
Dmitry Simonenko <pmwkaa@gmail.com>
2023-12-15 13:07:55 +0200

    runtime: update module header

--- noire/runtime/noire_runtime.h
+++ noire/runtime/noire_runtime.h
@@ -17,55 +17,46 @@
 #include "runtime/cond_var.h"
 #include "runtime/thread.h"
 
-#if 0
 // basic data structures
 #include "runtime/list.h"
-#include "runtime/utils.h"
+
+// exceptions
+#include "runtime/exception.h"
+#include "runtime/log.h"
+#include "runtime/error.h"
+
+// runtime
+#include "runtime/runtime.h"
+#include "runtime/throw.h"
+#include "runtime/guard.h"
 
 // memory
 #include "runtime/allocator.h"
 #include "runtime/str.h"
 #include "runtime/buf.h"
-#include "runtime/buf_cache.h"
-#include "runtime/buf_pool.h"
-#include "runtime/arena.h"
 
-// event loop
-#include "runtime/timer_mgr.h"
-#include "runtime/fd.h"
-#include "runtime/poller.h"
-#include "runtime/notify.h"
+#if 0
+// file io
+#include "io/iov.h"
+#include "io/vfs.h"
+#include "io/fs.h"
+#include "io/file.h"
+#include "io/logger.h"
 
-// exception
-#include "runtime/exception.h"
-#include "runtime/error.h"
-#include "runtime/log.h"
+// blob
+#include "io/blob.h"
 
-// cooperative multitasking
-#include "runtime/context_stack.h"
-#include "runtime/context.h"
-#include "runtime/event.h"
-#include "runtime/coroutine.h"
-#include "runtime/coroutine_mgr.h"
-#include "runtime/wait.h"
+// tls
+#include "io/tls_lib.h"
+#include "io/tls_context.h"
+#include "io/tls.h"
 
-// ipc
-#include "runtime/msg_id.h"
-#include "runtime/msg.h"
-#include "runtime/condition.h"
-#include "runtime/condition_cache.h"
-#include "runtime/channel.h"
-#include "runtime/task.h"
+// network io
+#include "io/socket.h"
+#include "io/poll.h"
+#include "io/tcp.h"
+#include "io/listen.h"
 
-// runtime
-#include "runtime/throw.h"
-#include "runtime/guard.h"
-#include "runtime/runtime.h"
-
-// rpc
-#include "runtime/rpc.h"
-
-// lock
-#include "runtime/locker.h"
-#include "runtime/lock.h"
+// resolver
+#include "io/resolver.h"
 #endif
