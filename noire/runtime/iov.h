29c52277774932a73ddd8d49f1f206f121ae6f87
Dmitry Simonenko <pmwkaa@gmail.com>
2023-12-15 13:20:33 +0200

    runtime: add iov object

---
+++ noire/runtime/iov.h
@@ -0,0 +1,62 @@
+#pragma once
+
+//
+// noire
+//
+// time-series storage
+//
+
+typedef struct Iov Iov;
+
+struct Iov
+{
+	Buf iov;
+	int iov_count;
+	int size;
+};
+
+static inline void
+iov_init(Iov* self)
+{
+	self->size  = 0;
+	self->iov_count = 0;
+	buf_init(&self->iov);
+}
+
+static inline void
+iov_free(Iov* self)
+{
+	buf_free(&self->iov);
+}
+
+static inline void
+iov_reset(Iov* self)
+{
+	self->size = 0;
+	self->iov_count = 0;
+	buf_reset(&self->iov);
+}
+
+static inline struct iovec*
+iov_pointer(Iov* self)
+{
+	return (struct iovec*)self->iov.start;
+}
+
+static inline void
+iov_reserve(Iov* self, int count)
+{
+	buf_reserve(&self->iov, sizeof(struct iovec) * count);
+}
+
+static inline void
+iov_add(Iov* self, void* pointer, int size)
+{
+	buf_reserve(&self->iov, sizeof(struct iovec));
+	auto iov = (struct iovec*)self->iov.position;
+	iov->iov_base = pointer;
+	iov->iov_len  = size;
+	buf_advance(&self->iov, sizeof(struct iovec));
+	self->iov_count++;
+	self->size += size;
+}
