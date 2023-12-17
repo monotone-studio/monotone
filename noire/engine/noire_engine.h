432a55533803ce1525a3be923d35a2b282581766
Dmitry Simonenko <pmwkaa@gmail.com>
2023-12-17 12:30:21 +0200

    engine: add module header

---
+++ noire/engine/noire_engine.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//
+// noire
+//
+// time-series storage
+//
+
+// engine
+#include "engine/engine.h"
+#include "engine/engine_recover.h"
+#if 0
+#include "engine/engine_cursor.h"
+#endif
+
+// compaction
+#include "engine/compaction.h"
+#include "engine/compaction_mgr.h"
