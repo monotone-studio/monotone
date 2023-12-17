953c2856509b39bd05b6aa4e91b5c737c59727df
Dmitry Simonenko <pmwkaa@gmail.com>
2023-12-17 12:29:32 +0200

    runtime: fix pthread argument

--- noire/runtime/thread.h
+++ noire/runtime/thread.h
@@ -31,7 +31,7 @@ thread_create(Thread* self, ThreadFunction function, void* arg)
 	self->function = function;
 	self->arg = arg;
 	int rc;
-	rc = pthread_create(&self->id, NULL, function, self);
+	rc = pthread_create(&self->id, NULL, function, self->arg);
 	if (rc != 0)
 		return -1;
 	return 0;
