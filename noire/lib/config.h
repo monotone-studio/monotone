c1975a77bc0bb2d44b14ad72e3b96759376d35f1
Dmitry Simonenko <pmwkaa@gmail.com>
2023-12-17 12:29:53 +0200

    lib: add config options

--- noire/lib/config.h
+++ noire/lib/config.h
@@ -10,13 +10,24 @@ typedef struct Config Config;
 
 struct Config
 {
-	// main
-	Str directory;
+	Str      directory;
+	uint64_t interval;
+	uint32_t compression;
+	uint32_t crc;
+	uint32_t region_size;
+	uint32_t compaction_wm;
+	uint32_t workers;
 };
 
 static inline void
 config_init(Config* self)
 {
+	self->interval      = 1048576;
+	self->compression   = COMPRESSION_OFF;
+	self->crc           = 0;
+	self->region_size   = 128 * 1024;
+	self->compaction_wm = 40 * 1024 * 1024;
+	self->workers       = 1;
 	str_init(&self->directory);
 }
 
