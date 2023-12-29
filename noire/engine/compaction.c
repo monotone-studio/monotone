efc04458cc6242470024145dfdeb7279fe1c26b2
Dmitry Simonenko <pmwkaa@gmail.com>
2023-12-29 11:05:14 +0200

    engine: add tiering/rebalance and drop logic to compaction

--- noire/engine/compaction.c
+++ noire/engine/compaction.c
@@ -11,7 +11,7 @@
 #include <noire_engine.h>
 
 hot static inline void
-compaction_execute(Compaction* self, ServiceReq* req)
+compaction_execute(Compaction* self, ServiceReq* req, Tier* tier)
 {
 	auto engine = self->engine;
 
@@ -24,33 +24,69 @@ compaction_execute(Compaction* self, ServiceReq* req)
 
 	// stage 2. create new partition by merging existing
 	//          partition file with the memtable
+	Storage* storage;
+	if (tier) {
+		storage = tier->storage;
+	} else {
+		tier = tier_of(&engine->tier_mgr, origin->storage->order);
+		storage = origin->storage;
+	}
+
+	if (storage->drop)
+	{
+		auto lock = engine_find(engine, false, req->min);
+		assert(lock);
+
+		auto origin_tier = tier_of(&engine->tier_mgr, origin->storage->order);
+
+		mutex_lock(&engine->lock);
+		part_tree_remove(&engine->tree, origin);
+		list_unlink(&origin->link);
+		engine->list_count--;
+
+		tier_remove(origin_tier, origin);
+		mutex_unlock(&engine->lock);
+
+		lock_mgr_unlock(lock);
+
+		// remove and free old partition
+		if (origin->storage->memory)
+			part_unload(origin);
+		part_delete(origin, true);
+		part_free(origin);
+		return;
+	}
+
 	MergerReq merger_req =
 	{
-		.origin      = origin,
-		.memtable    = memtable,
-		.directory   = config_directory(),
-		.compression = config()->compression,
-		.crc         = config()->crc,
-		.region_size = config()->region_size
+		.origin   = origin,
+		.memtable = memtable,
+		.storage  = storage
 	};
 	merger_reset(&self->merger);
 	auto part = merger_execute(&self->merger, &merger_req);
 	part->service = true;
 
-	// stage 3. replace existing partition with the new one,
-	//          reuse current memtable
+	// stage 3. replace existing partition with the new one
 	lock = engine_find(engine, false, req->min);
 	assert(lock);
 	assert(lock->arg == origin);
 
 	mutex_lock(&engine->lock);
 
+	// update partition tree and list
 	part_tree_replace(&engine->tree, origin, part);
 	list_unlink(&origin->link);
 	list_append(&engine->list, &part->link);
 
+	// update tiers
+	auto origin_tier = tier_of(&engine->tier_mgr, origin->storage->order);
+	tier_remove(origin_tier, origin);
+	tier_add(tier, part);
+
 	mutex_unlock(&engine->lock);
 
+	// reuse memtable
 	*part->memtable = *origin->memtable;
 	memtable_init(origin->memtable, origin->comparator);
 
@@ -62,15 +98,21 @@ compaction_execute(Compaction* self, ServiceReq* req)
 	memtable_free(memtable);
 	memtable_init(memtable, NULL);
 
-	// sync new partition
-	/*part_sync(part);*/
+	// create partition file and sync
+	part_create(part);
+
+	if (storage->sync)
+		part_sync(part);
+
+	if (! storage->memory)
+		part_unload(part);
 
 	// remove and free old partition
-	/*part_delete(origin, true);*/
+	part_delete(origin, true);
 	part_free(origin);
 
 	// rename new partition
-	/*part_complete(part);*/
+	part_complete(part);
 
 	// make new partition available for service
 	mutex_lock(&engine->lock);
@@ -78,11 +120,64 @@ compaction_execute(Compaction* self, ServiceReq* req)
 	mutex_unlock(&engine->lock);
 }
 
+hot static inline bool
+compaction_rebalance_next(Compaction* self, ServiceReq* req, Tier** req_tier)
+{
+	auto engine = self->engine;
+	mutex_lock(&engine->lock);
+
+	bool match = false;
+	for (int order = 0; order < engine->tier_mgr.tiers_count; order++)
+	{
+		auto tier = tier_of(&engine->tier_mgr, order);
+		auto storage = tier->storage;
+		if (! storage->capacity)
+			break;
+
+		assert(engine->tier_mgr.tiers_count > order + 1);
+		if (tier->list_count >= storage->capacity)
+		{
+			// find min partition
+			auto part = tier_min(tier);
+			if (! part->service)
+			{
+				req->min  = part->min;
+				req->max  = part->max;
+				*req_tier = tier_of(&engine->tier_mgr, order + 1);
+				part->service = true;
+				match = true;
+				break;
+			}
+		}
+	}
+
+	mutex_unlock(&engine->lock);
+	return match;
+}
+
+hot static inline void
+compaction_rebalance(Compaction* self)
+{
+	if (self->engine->tier_mgr.tiers_count == 1)
+		return;
+
+	for (;;)
+	{
+		ServiceReq req;
+		service_req_init(&req);
+		Tier* tier = NULL;
+		if (! compaction_rebalance_next(self, &req, &tier))
+			break;
+		compaction_execute(self, &req, tier);
+	}
+}
+
 static void*
 compaction_main(void* arg)
 {
 	Compaction* self = arg;
-	runtime_init(self->global);
+	runtime_init();
+	runtime_set_global(self->global);
 
 	thread_set_name(&self->thread, "compaction");
 	for (;;)
@@ -92,11 +187,19 @@ compaction_main(void* arg)
 			break;
 
 		Exception e;
-		if (try(&e)) {
-			compaction_execute(self, req);
+		if (try(&e))
+		{
+			// rebalance tiers first
+			compaction_rebalance(self);
+
+			// compaction
+			if (! service_req_rebalance(req))
+				compaction_execute(self, req, NULL);
 		}
 		if (catch(&e))
 		{ }
+
+		service_req_free(req);
 	}
 
 	return NULL;
