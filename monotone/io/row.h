39c033834ea02d25b3859f61679fc2a465dd3c6e
Dmitry Simonenko <pmwkaa@gmail.com>
2023-12-31 13:57:54 +0200

    io: add row_allocate/free()

--- monotone/io/row.h
+++ monotone/io/row.h
@@ -10,9 +10,9 @@ typedef struct Row Row;
 
 struct Row
 {
+	uint64_t time;
 	uint8_t  is_delete:1;
 	uint8_t  flags:7;
-	uint64_t time;
 	uint32_t data_size;
 	uint8_t  data[];
 } packed;
@@ -20,12 +20,27 @@ struct Row
 hot static inline Row*
 row_init(Row* self, uint64_t time, uint8_t* data, int data_size)
 {
+	self->time      = time;
 	self->is_delete = 0;
 	self->flags     = 0;
-	self->time      = time;
 	self->data_size = data_size;
-	memcpy(self->data, data, data_size);
+	if (data)
+		memcpy(self->data, data, data_size);
 	return self;
+}
+
+static inline Row*
+row_allocate(uint64_t time, uint8_t* data, int data_size)
+{
+	auto row = (Row*)mn_malloc(sizeof(Row) + data_size);
+	row_init(row, time, data, data_size);
+	return row;
+}
+
+always_inline static inline void
+row_free(Row* self)
+{
+	mn_free(self);
 }
 
 always_inline hot static inline int
