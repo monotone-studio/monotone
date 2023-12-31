d19aa6d3996984e0064d25368d5d2a32f6b9b215
Dmitry Simonenko <pmwkaa@gmail.com>
2023-12-31 13:57:09 +0200

    io: redesign memtable as T-tree style storage

--- monotone/io/memtable.h
+++ monotone/io/memtable.h
@@ -6,101 +6,31 @@
 // time-series storage
 //
 
-typedef struct MemtableRow MemtableRow;
-typedef struct Memtable    Memtable;
+typedef struct MemtablePage MemtablePage;
+typedef struct Memtable     Memtable;
 
-struct MemtableRow
+struct MemtablePage
 {
 	RbtreeNode node;
-	Row        row;
+	int        keys_count;
+	Row*       keys[];
 };
 
 struct Memtable
 {
+	Comparator* comparator;
+	Rbtree      tree;
 	uint64_t    count;
+	int         count_pages;
 	uint64_t    size;
+	int         size_page;
+	int         size_split;
 	uint64_t    lsn_min;
 	uint64_t    lsn_max;
-	Rbtree      index;
-	Comparator* comparator;
 };
 
-hot static inline MemtableRow*
-memtable_row_allocate(uint64_t time, uint8_t* data, int data_size)
-{
-	MemtableRow* ref;
-	ref = mn_malloc(sizeof(MemtableRow) + data_size);
-	row_init(&ref->row, time, data, data_size);
-	rbtree_init_node(&ref->node);
-	return ref;
-}
-
-always_inline hot static inline void
-memtable_row_free(MemtableRow* self)
-{
-	mn_free(self);
-}
-
-always_inline static inline MemtableRow*
-memtable_row_of(RbtreeNode* node)
-{
-	return container_of(node, MemtableRow, node);
-}
-
-always_inline hot static inline int
-memtable_compare(Memtable* self, MemtableRow* a, MemtableRow* b)
-{
-	return compare(self->comparator, &a->row, &b->row);
-}
-
-static inline void
-memtable_init(Memtable* self, Comparator* comparator)
-{
-	self->count      = 0;
-	self->size       = 0;
-	self->comparator = comparator;
-	rbtree_init(&self->index);
-}
-
-rbtree_free(memtable_truncate, memtable_row_free(memtable_row_of(n)))
-
-static inline void
-memtable_free(Memtable* self)
-{
-	if (self->index.root)
-		memtable_truncate(self->index.root, NULL);
-}
-
-hot static inline void
-memtable_follow_lsn(Memtable* self, uint64_t lsn)
-{
-	if (lsn < self->lsn_min)
-		self->lsn_min = lsn;
-	if (lsn > self->lsn_max)
-		self->lsn_max = lsn;
-}
-
-hot static inline
-rbtree_get(memtable_match, memtable_compare(arg, memtable_row_of(n), key))
-
-hot static inline void
-memtable_set(Memtable* self, MemtableRow* row)
-{
-	RbtreeNode* node;
-	int rc;
-	rc = memtable_match(&self->index, self->comparator, row, &node);
-	if (rc == 0 && node)
-	{
-		// replace
-		auto head = memtable_row_of(node);
-		rbtree_replace(&self->index, &head->node, &row->node);
-		self->size -= row_size(&row->row) + sizeof(MemtableRow);
-		self->count--;
-	} else
-	{
-		// insert
-		rbtree_set(&self->index, node, rc, &row->node);
-	}
-	self->size += row_size(&row->row) + sizeof(MemtableRow);
-	self->count++;
-}
+void memtable_init(Memtable*, int, int, Comparator*);
+void memtable_free(Memtable*);
+void memtable_set(Memtable*, Row*);
+bool memtable_seek(Memtable*, Row*, MemtablePage**, int*);
+void memtable_follow(Memtable*, uint64_t);
