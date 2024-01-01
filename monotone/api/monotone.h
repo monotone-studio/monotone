cdf7d826cbb6404278bc615b3a1d6bd0e3c7badc
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-01 15:21:40 +0200

    api: update api functions

--- monotone/api/monotone.h
+++ monotone/api/monotone.h
@@ -19,17 +19,21 @@ extern "C" {
 
 typedef struct monotone        monotone_t;
 typedef struct monotone_cursor monotone_cursor_t;
+typedef struct monotone_row    monotone_row_t;
 
-typedef struct
+typedef int64_t (*monotone_compare_t)(uint64_t l_time, void* l,
+                                      uint64_t r_time, void* r,
+                                      void*    arg);
+struct monotone_row
 {
 	uint64_t time;
 	void*    data;
 	size_t   data_size;
-} monotone_row_t;
+};
 
 // main
 MONOTONE_API monotone_t*
-monotone_init(void);
+monotone_init(monotone_compare_t, void* compare_arg);
 
 MONOTONE_API void
 monotone_free(void*);
@@ -37,27 +41,20 @@ monotone_free(void*);
 MONOTONE_API const char*
 monotone_error(monotone_t*);
 
+MONOTONE_API int
+monotone_version(void);
+
 MONOTONE_API uint64_t
 monotone_now(monotone_t*);
 
-// configuration
-MONOTONE_API void
-monotone_set(monotone_t*, const char* var, intptr_t value);
-
-MONOTONE_API intptr_t
-monotone_show(monotone_t*, const char* var);
-
-// todo: monitoring
-
 // storage
 MONOTONE_API int
-monotone_tier(monotone_t*, const char* name);
+monotone_open(monotone_t*, const char* options);
 
 MONOTONE_API int
-monotone_open(monotone_t*, const char* directory);
+monotone_checkpoint(monotone_t*, uint64_t before);
 
-MONOTONE_API int
-monotone_checkpoint(monotone_t*, uint64_t older_then);
+// todo: monitoring
 
 // cursor
 MONOTONE_API monotone_cursor_t*
@@ -84,7 +81,7 @@ monotone_delete_as(monotone_t*, monotone_row_t*);
 
 // partitions
 MONOTONE_API int
-monotone_drop_partitions(monotone_t*, uint64_t min, uint64_t max);
+monotone_drop(monotone_t*, uint64_t min, uint64_t max);
 
 #ifdef __cplusplus
 }
