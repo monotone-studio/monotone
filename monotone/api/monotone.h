01228d13827ffbcfd2ceeb38d17823e1f20603e5
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-03 15:56:30 +0200

    api: add stats to the api

--- monotone/api/monotone.h
+++ monotone/api/monotone.h
@@ -17,13 +17,40 @@ extern "C" {
 
 #define MONOTONE_API __attribute__((visibility("default")))
 
-typedef struct monotone        monotone_t;
-typedef struct monotone_cursor monotone_cursor_t;
-typedef struct monotone_row    monotone_row_t;
+typedef struct monotone               monotone_t;
+typedef struct monotone_stats         monotone_stats_t;
+typedef struct monotone_stats_storage monotone_stats_storage_t;
+typedef struct monotone_cursor        monotone_cursor_t;
+typedef struct monotone_row           monotone_row_t;
 
 typedef int64_t (*monotone_compare_t)(uint64_t l_time, void* l,
                                       uint64_t r_time, void* r,
                                       void*    arg);
+
+struct monotone_stats
+{
+	uint64_t lsn;
+	uint64_t rows_written;
+	uint64_t rows_written_bytes;
+	uint32_t storages;
+	uint64_t reserved[8];
+};
+
+struct monotone_stats_storage
+{
+	const char* name;
+	uint64_t    partitions;
+	uint64_t    pending;
+	uint64_t    min;
+	uint64_t    max;
+	uint64_t    rows;
+	uint64_t    rows_cached;
+	uint64_t    size;
+	uint64_t    size_uncompressed;
+	uint64_t    size_cached;
+	uint64_t    reserved[8];
+};
+
 struct monotone_row
 {
 	uint64_t time;
@@ -54,7 +81,8 @@ monotone_open(monotone_t*, const char* options);
 MONOTONE_API int
 monotone_checkpoint(monotone_t*, uint64_t before);
 
-// todo: monitoring
+MONOTONE_API monotone_stats_storage_t*
+monotone_stats(monotone_t*, monotone_stats_t*);
 
 // cursor
 MONOTONE_API monotone_cursor_t*
