7378348a55c228b61d4185c22984a4027f8c06cb
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-04 14:35:55 +0200

    io: introduce partition index file

--- monotone/io/part.c
+++ monotone/io/part.c
@@ -9,55 +9,95 @@
 #include <monotone_lib.h>
 #include <monotone_io.h>
 
-static inline void
-part_path(char* path, Storage* storage, uint64_t min, uint64_t max)
+Part*
+part_allocate(Comparator* comparator,
+              Storage*    storage,
+              uint64_t    min,
+              uint64_t    max)
 {
-	snprintf(path, PATH_MAX, "%s/%020" PRIu64 ".%020" PRIu64,
-	         str_of(&storage->path),
-	         min, max);
+	auto self = (Part*)mn_malloc(sizeof(Part));
+	self->min        = min;
+	self->max        = max;
+	self->service    = false;
+	self->index      = NULL;
+	self->storage    = storage;
+	self->comparator = comparator;
+	blob_init(&self->mmap, 1 * 1024 * 1024);
+	file_init(&self->file);
+	self->memtable = &self->memtable_a;
+	memtable_init(&self->memtable_a, 512, 508, comparator);
+	memtable_init(&self->memtable_b, 512, 508, comparator);
+	buf_init(&self->index_buf);
+	rbtree_init_node(&self->node);
+	list_init(&self->link_tier);
+	list_init(&self->link);
+	return self;
 }
 
 static inline void
-part_path_incomplete(char* path, Storage* storage, uint64_t min, uint64_t max)
+part_free_data(Part* self)
 {
-	snprintf(path, PATH_MAX, "%s/%020" PRIu64 ".%020" PRIu64 ".incomplete",
+	memtable_free(&self->memtable_a);
+	memtable_free(&self->memtable_b);
+	blob_free(&self->mmap);
+	file_close(&self->file);
+	buf_free(&self->index_buf);
+	buf_init(&self->index_buf);
+}
+
+void
+part_free(Part* self)
+{
+	part_free_data(self);
+	mn_free(self);
+}
+
+static inline void
+part_path(char* path, Storage* storage, uint64_t min, uint64_t max, bool index)
+{
+	snprintf(path, PATH_MAX, "%s/%020" PRIu64 ".%020" PRIu64 "%s",
 	         str_of(&storage->path),
-	         min, max);
+	         min, max,
+	         index ? ".index" : "");
+}
+
+static inline void
+part_path_incomplete(char* path, Storage* storage, uint64_t min, uint64_t max, bool index)
+{
+	snprintf(path, PATH_MAX, "%s/%020" PRIu64 ".%020" PRIu64 "%s.incomplete",
+	         str_of(&storage->path),
+	         min, max,
+	         index ? ".index" : "");
 }
 
 void
 part_open(Part* self, bool check_crc)
 {
-	// open data file and read index
+	// open data file
 	char path[PATH_MAX];
-	part_path(path, self->storage, self->min, self->max);
+	part_path(path, self->storage, self->min, self->max, false);
 	file_open(&self->file, path);
 
-	if (unlikely(self->file.size < (sizeof(Index) + sizeof(IndexEof))))
-		goto corrupted;
+	// open index file
+	part_path(path, self->storage, self->min, self->max, true);
 
-	// [data][index][index_eof]
-	IndexEof eof;
-	file_pread(&self->file, &eof, sizeof(eof),
-	            self->file.size - sizeof(IndexEof));
+	File file;
+	file_init(&file);
+	guard(guard, file_close, &file);
+	file_open(&self->file, path);
 
-	// check magic
-	if (unlikely(eof.magic != INDEX_MAGIC))
-		goto corrupted;
+	if (file.size < sizeof(Index))
+		error("partition: index file '%s' has incorrect size",
+		      str_of(&file.path));
 
-	// validate index offset
-	int64_t offset = self->file.size - (sizeof(IndexEof) + eof.size);
-	if (offset < 0)
-		goto corrupted;
+	// read and validate index
+	file_pread_buf(&file, &self->index_buf, file.size, 0);
 
-	// read index
-	file_pread_buf(&self->file, &self->index_buf, eof.size, offset);
-
-	// set and validate index
+	// check index header size
 	self->index = (Index*)self->index_buf.start;
-
-	if (offset != (int64_t)self->index->size_total)
-		goto corrupted;
+	if (file.size != self->index->size)
+		error("partition: index file '%s' size mismatch",
+		      str_of(&file.path));
 
 	// check crc
 	if (check_crc)
@@ -66,56 +106,83 @@ part_open(Part* self, bool check_crc)
 		crc = crc32(0, self->index_buf.start + sizeof(uint32_t),
 		            buf_size(&self->index_buf) - sizeof(uint32_t));
 		if (crc != self->index->crc)
-			goto corrupted;
+			error("partition: index file '%s' crc mismatch",
+			      str_of(&file.path));
 	}
 
-	return;
-
-corrupted:
-	error("partition: file '%s' is corrupted", self->file.path);
+	// check data file size
+	if (file.size != self->index->size_total)
+		error("partition: data file '%s' size mismatch",
+		      str_of(&self->file.path));
 }
 
 void
-part_create(Part* self)
+part_create(Part* self, bool sync)
 {
+	assert(self->mmap.start);
+
+	// create min.max.incomplete file
 	char path[PATH_MAX];
-	part_path_incomplete(path, self->storage, self->min, self->max);
+	part_path_incomplete(path, self->storage, self->min, self->max, false);
 	file_create(&self->file, path);
-	if (self->mmap.start)
-		file_write(&self->file, self->mmap.start, blob_size(&self->mmap));
-	file_write(&self->file, self->index_buf.start, buf_size(&self->index_buf));
+	file_write(&self->file, self->mmap.start, blob_size(&self->mmap));
+
+	// create min.max.index.incomplete file
+	part_path_incomplete(path, self->storage, self->min, self->max, true);
+
+	File file;
+	file_init(&file);
+	guard(guard, file_close, &file);
+	file_create(&file, path);
+	file_write(&file, self->index_buf.start, buf_size(&self->index_buf));
+
+	// sync files
+	if (sync)
+	{
+		file_sync(&self->file);
+		file_sync(&file);
+	}
+}
+
+void
+part_delete(Part* self, bool complete)
+{
+	// delete min.max.index file
+	char path[PATH_MAX];
+	if (complete)
+		part_path(path, self->storage, self->min, self->max, true);
+	else
+		part_path_incomplete(path, self->storage, self->min, self->max, true);
+	if (fs_exists("%s", path))
+		fs_unlink("%s", path);
+
+	// delete min.max file
+	if (complete)
+		part_path(path, self->storage, self->min, self->max, false);
+	else
+		part_path_incomplete(path, self->storage, self->min, self->max, false);
+	if (fs_exists("%s", path))
+		fs_unlink("%s", path);
+}
+
+void
+part_rename(Part* self)
+{
+	// rename to min.max.index file
+	char path[PATH_MAX];
+	char path_to[PATH_MAX];
+	part_path_incomplete(path, self->storage, self->min, self->max, true);
+	part_path(path_to, self->storage, self->min, self->max, true);
+	fs_rename(path, "%s", path_to);
+
+	// rename to min.max file
+	part_path_incomplete(path, self->storage, self->min, self->max, false);
+	part_path(path_to, self->storage, self->min, self->max, false);
+	fs_rename(path, "%s", path_to);
 }
 
 void
 part_unload(Part* self)
 {
 	blob_free(&self->mmap);
-}
-
-void
-part_sync(Part* self)
-{
-	file_sync(&self->file);
-}
-
-void
-part_delete(Part* self, bool complete)
-{
-	char path[PATH_MAX];
-	if (complete)
-		part_path(path, self->storage, self->min, self->max);
-	else
-		part_path_incomplete(path, self->storage, self->min, self->max);
-	if (fs_exists("%s", path))
-		fs_unlink("%s", path);
-}
-
-void
-part_complete(Part* self)
-{
-	char path[PATH_MAX];
-	char path_to[PATH_MAX];
-	part_path_incomplete(path, self->storage, self->min, self->max);
-	part_path(path_to, self->storage, self->min, self->max);
-	fs_rename(path, "%s", path_to);
 }
--- monotone/io/part.h
+++ monotone/io/part.h
@@ -27,48 +27,14 @@ struct Part
 	List        link;
 };
 
-static inline Part*
-part_allocate(Comparator* comparator,
-              Storage*    storage,
-              uint64_t    min,
-              uint64_t    max)
-{
-	auto self = (Part*)mn_malloc(sizeof(Part));
-	self->min        = min;
-	self->max        = max;
-	self->service    = false;
-	self->index      = NULL;
-	self->storage    = storage;
-	self->comparator = comparator;
-	blob_init(&self->mmap, 1 * 1024 * 1024);
-	file_init(&self->file);
-	self->memtable = &self->memtable_a;
-	memtable_init(&self->memtable_a, 512, 508, comparator);
-	memtable_init(&self->memtable_b, 512, 508, comparator);
-	buf_init(&self->index_buf);
-	rbtree_init_node(&self->node);
-	list_init(&self->link_tier);
-	list_init(&self->link);
-	return self;
-}
-
-static inline void
-part_free_data(Part* self)
-{
-	memtable_free(&self->memtable_a);
-	memtable_free(&self->memtable_b);
-	blob_free(&self->mmap);
-	file_close(&self->file);
-	buf_free(&self->index_buf);
-	buf_init(&self->index_buf);
-}
-
-static inline void
-part_free(Part* self)
-{
-	part_free_data(self);
-	mn_free(self);
-}
+Part*
+part_allocate(Comparator*, Storage*, uint64_t, uint64_t);
+void part_free(Part*);
+void part_open(Part*, bool);
+void part_create(Part*, bool);
+void part_delete(Part*, bool);
+void part_rename(Part*);
+void part_unload(Part*);
 
 static inline Memtable*
 part_memtable_of(Part* self, Memtable** prev)
@@ -92,10 +58,3 @@ part_memtable_rotate(Part* self)
 	self->memtable = current;
 	return prev;
 }
-
-void part_open(Part*, bool);
-void part_create(Part*);
-void part_unload(Part*);
-void part_sync(Part*);
-void part_delete(Part*, bool);
-void part_complete(Part*);
