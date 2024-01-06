4bc81a8054f6daede6450e44a8e8cc8fcdfc2fe8
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-06 16:56:34 +0200

    engine: update compaction header

--- monotone/engine/compaction.h
+++ monotone/engine/compaction.h
@@ -21,3 +21,5 @@ void compaction_init(Compaction*);
 void compaction_free(Compaction*);
 void compaction_start(Compaction*, Service*, Engine*);
 void compaction_stop(Compaction*);
+void compaction_execute(Compaction*, ServiceReq*, Tier*);
+void compaction_rebalance(Compaction*);
