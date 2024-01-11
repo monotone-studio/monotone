87cb436427ee5b563cfed3c3b8be64171c98c271
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-11 13:18:12 +0200

    storage: add module header

---
+++ monotone/storage/monotone_storage.h
@@ -0,0 +1,23 @@
+#pragma once
+
+//
+// monotone
+//
+// time-series storage
+//
+
+// storage
+#include "storage/storage.h"
+
+#if 0
+// storage
+
+// tiering
+#include "io/tier.h"
+
+// pipeline
+
+// service
+#include "io/service_req.h"
+#include "io/service.h"
+#endif
