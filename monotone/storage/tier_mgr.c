7aee954b10aaca4bb7039b2f9ea87164eaf834e8
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-11 15:46:47 +0200

    storage: add tier manager

---
+++ monotone/storage/tier_mgr.c
@@ -0,0 +1,127 @@
+
+//
+// monotone
+//
+// time-series storage
+//
+
+#include <monotone_runtime.h>
+#include <monotone_lib.h>
+#include <monotone_io.h>
+#include <monotone_storage.h>
+
+void
+tier_mgr_init(TierMgr* self, StorageMgr* storage_mgr)
+{
+	self->storage_mgr = storage_mgr;
+	self->list_count  = 0;
+	list_init(&self->list);
+}
+
+void
+tier_mgr_free(TierMgr* self)
+{
+	list_foreach_safe(&self->list)
+	{
+		auto tier = list_at(Tier, link);
+		tier_free(tier);
+	}
+}
+
+static inline void
+tier_mgr_save(TierMgr* self)
+{
+	Buf buf;
+	buf_init(&buf);
+	guard(guard, buf_free, &buf);
+
+	// create dump
+	encode_array(&buf, self->list_count);
+	list_foreach(&self->list)
+	{
+		auto tier = list_at(Tier, link);
+		tier_config_write(tier->config, &buf);
+	}
+
+	// update state
+	var_data_set_buf(&config()->tiers, &buf);
+}
+
+void
+tier_mgr_open(TierMgr* self)
+{
+	auto tiers = &config()->tiers;
+	if (! var_data_is_set(tiers))
+		return;
+	auto pos = var_data_of(tiers);
+	if (data_is_null(pos))
+		return;
+
+	int count;
+	data_read_array(&pos, &count);
+	for (int i = 0; i < count; i++)
+	{
+		// create tier config
+		auto config = tier_config_read(&pos);
+		guard(guard, tier_config_free, config);
+
+		// create tier
+		auto tier = tier_allocate(config);
+		list_append(&self->list, &tier->link);
+		self->list_count++;
+
+		// todo: set storages
+	}
+}
+
+void
+tier_mgr_create(TierMgr* self, TierConfig* config, bool if_not_exists)
+{
+	auto tier = tier_mgr_find(self, &config->name);
+	if (tier)
+	{
+		if (! if_not_exists)
+			error("tier '%.*s': already exists", str_size(&config->name),
+			      str_of(&config->name));
+		return;
+	}
+	tier = tier_allocate(config);
+	// todo: link storages
+
+	list_append(&self->list, &tier->link);
+	self->list_count++;
+
+	tier_mgr_save(self);
+}
+
+void
+tier_mgr_drop(TierMgr* self, Str* name, bool if_exists)
+{
+	auto tier = tier_mgr_find(self, name);
+	if (! tier)
+	{
+		if (! if_exists)
+			error("tier '%.*s': not exists", str_size(name), str_of(name));
+		return;
+	}
+	// todo: unlink storages
+
+	list_unlink(&tier->link);
+	self->list_count--;
+	assert(self->list_count >= 0);
+	tier_free(tier);
+
+	tier_mgr_save(self);
+}
+
+Tier*
+tier_mgr_find(TierMgr* self, Str* name)
+{
+	list_foreach(&self->list)
+	{
+		auto tier = list_at(Tier, link);
+		if (str_compare(&tier->config->name, name))
+			return tier;
+	}
+	return NULL;
+}
