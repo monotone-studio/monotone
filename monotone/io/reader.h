0fd997c8eb989c0b373d2e409b309c79f7ce30d4
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-11 11:51:40 +0200

    io: add Reader

---
+++ monotone/io/reader.h
@@ -0,0 +1,66 @@
+#pragma once
+
+//
+// monotone
+//
+// time-series storage
+//
+
+typedef struct Reader Reader;
+
+struct Reader
+{
+	Index*       index;
+	IndexRegion* index_region;
+	Region*      region;
+	Buf*         read_buf;
+	Buf*         read_buf_uncompressed;
+	File*        file;
+};
+
+static inline void
+reader_init(Reader* self)
+{
+	self->index                 = NULL;
+	self->index_region          = NULL;
+	self->region                = NULL;
+	self->read_buf              = NULL;
+	self->read_buf_uncompressed = NULL;
+	self->file                  = NULL;
+}
+
+static inline void
+reader_reset(Reader* self)
+{
+	self->index                 = NULL;
+	self->index_region          = NULL;
+	self->region                = NULL;
+	self->read_buf              = NULL;
+	self->read_buf_uncompressed = NULL;
+	self->file                  = NULL;
+}
+
+hot static inline void
+reader_execute(Reader* self)
+{
+	buf_reset(self->read_buf);
+	buf_reset(self->read_buf_uncompressed);
+
+	// read region data from file
+	file_pread_buf(self->file, self->read_buf, self->index_region->size,
+	               self->index_region->offset);
+
+	self->region = (Region*)self->read_buf->start;
+
+	// decompress region
+	int compression = self->index->compression;
+	if (compression_is_set(compression))
+	{
+		compression_decompress(self->read_buf_uncompressed,
+		                       compression,
+		                       self->index_region->size_origin,
+		                       (char*)self->region,
+		                       self->index_region->size);
+		self->region = (Region*)self->read_buf_uncompressed->start;
+	}
+}
