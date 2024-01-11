9aff47374eda8b647872105ae0ffeaccf5396df6
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-11 13:26:38 +0200

    storage: add Storage object

---
+++ monotone/storage/storage.h
@@ -0,0 +1,80 @@
+#pragma once
+
+//
+// monotone
+//
+// time-series storage
+//
+
+typedef struct Storage Storage;
+
+struct Storage
+{
+	List    list;
+	int     list_count;
+	Target* target;
+	List    link;
+};
+
+static inline void
+storage_free(Storage* self)
+{
+	if (self->target)
+		target_free(self->target);
+	mn_free(self);
+}
+
+static inline Storage*
+storage_allocate(Target* target)
+{
+	Storage* self = mn_malloc(sizeof(Storage));
+	self->target     = NULL;
+	self->list_count = 0;
+	list_init(&self->list);
+	guard(self_guard, storage_free, self);
+	self->target     = target_copy(target);
+	return unguard(&self_guard);
+}
+
+static inline void
+storage_add(Storage* self, Part* part)
+{
+	list_append(&self->list, &part->link_tier);
+	self->list_count++;
+}
+
+static inline void
+storage_remove(Storage* self, Part* part)
+{
+	assert(part->target == self->target);
+	list_unlink(&part->link_tier);
+	self->list_count--;
+}
+
+hot static inline Part*
+storage_find(Storage* self, uint64_t min)
+{
+	list_foreach(&self->list)
+	{
+		auto part = list_at(Part, link_tier);
+		if (part->min == min)
+			return part;
+	}
+	return NULL;
+}
+
+hot static inline Part*
+storage_min(Storage* self)
+{
+	Part* min = NULL;
+	list_foreach(&self->list)
+	{
+		auto part = list_at(Part, link_tier);
+		if (min == NULL)
+			min = part;
+		else
+		if (part->min < min->min)
+			min = part;
+	}
+	return min;
+}
