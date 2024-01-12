5dbeb45f25d451d4b10c1d21d59f1e50d570b590
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-12 16:19:14 +0200

    io: add service req object

---
+++ monotone/io/service_req.h
@@ -0,0 +1,46 @@
+#pragma once
+
+//
+// monotone
+//
+// time-series storage
+//
+
+typedef struct ServiceReq ServiceReq;
+
+struct ServiceReq
+{
+	uint64_t min;
+	uint64_t max;
+	List     link;
+};
+
+static inline void
+service_req_init(ServiceReq* self)
+{
+	self->min = 0;
+	self->max = 0;
+	list_init(&self->link);
+}
+
+static inline ServiceReq*
+service_req_allocate(int64_t min, uint64_t max)
+{
+	auto self = (ServiceReq*)mn_malloc(sizeof(ServiceReq));
+	service_req_init(self);
+	self->min = min;
+	self->max = max;
+	return self;
+}
+
+static inline void
+service_req_free(ServiceReq* self)
+{
+	mn_free(self);
+}
+
+static inline bool
+service_req_rebalance(ServiceReq* self)
+{
+	return self->min == 0 && self->max == 0;
+}
