119bd47b8d15e10e7f2a5926895d52edb0a5fe87
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-12 10:21:19 +0200

    storage: add conveyor object

---
+++ monotone/storage/conveyor.h
@@ -0,0 +1,22 @@
+#pragma once
+
+//
+// monotone
+//
+// time-series storage
+//
+
+typedef struct Conveyor Conveyor;
+
+struct Conveyor
+{
+	List        list;
+	int         list_count;
+	StorageMgr* storage_mgr;
+};
+
+void conveyor_init(Conveyor*, StorageMgr*);
+void conveyor_free(Conveyor*);
+void conveyor_open(Conveyor*);
+void conveyor_create(Conveyor*, List*, bool);
+void conveyor_drop(Conveyor*, bool);
