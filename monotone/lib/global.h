68efa8e9068cb7df4f738a6d931c3549356ad20b
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-14 10:42:33 +0200

    lib: add psn support

--- monotone/lib/global.h
+++ monotone/lib/global.h
@@ -23,6 +23,15 @@ config_directory(void)
 	return var_string_of(&config()->directory);
 }
 
+static inline void
+config_update(void)
+{
+	char path[PATH_MAX];
+	snprintf(path, sizeof(path), "%s/config.json",
+	         config_directory());
+	config_save(config(), path);
+}
+
 static inline bool
 config_online(void)
 {
@@ -35,11 +44,27 @@ config_interval(void)
 	return var_int_of(&config()->interval);
 }
 
-static inline void
-config_update(void)
+// psn
+static inline uint64_t
+config_psn(void)
 {
-	char path[PATH_MAX];
-	snprintf(path, sizeof(path), "%s/config.json",
-	         config_directory());
-	config_save(config(), path);
+	return var_int_of(&config()->psn);
+}
+
+static inline void
+config_psn_set(uint64_t value)
+{
+	var_int_set(&config()->psn, value);
+}
+
+static inline uint64_t
+config_psn_next(void)
+{
+	return var_int_set_next(&config()->psn);
+}
+
+static inline void
+config_psn_follow(uint64_t value)
+{
+	var_int_follow(&config()->psn, value);
 }
