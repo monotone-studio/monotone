05c53b04e2967bf900d703d1016bf9c30e2a843f
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-15 14:13:29 +0200

    storage: implement storage stats

---
+++ monotone/storage/storage_stats.h
@@ -0,0 +1,80 @@
+#pragma once
+
+//
+// monotone
+//
+// time-series storage
+//
+
+typedef struct StorageStats StorageStats;
+
+struct StorageStats
+{
+	uint64_t partitions;
+	uint64_t min;
+	uint64_t max;
+	uint64_t rows;
+	uint64_t rows_cached;
+	uint64_t size;
+	uint64_t size_uncompressed;
+	uint64_t size_cached;
+};
+
+static inline void
+storage_stats_init(StorageStats* self)
+{
+	memset(self, 0, sizeof(*self));
+}
+
+hot static inline void
+storage_stats(Storage* self, StorageStats* stats)
+{
+	stats->partitions = self->list_count;
+	stats->min        = UINT64_MAX;
+	stats->max        = 0;
+	list_foreach(&self->list)
+	{
+		auto part = list_at(Part, link);
+
+		// min/max
+		if (part->min < stats->min)
+			stats->min = part->min;
+		if (part->max > stats->max)
+			stats->max = part->max;
+
+		// rows
+		if (part->index)
+			stats->rows += part->index->count_total;
+		uint64_t rows_cached = part->memtable_a.count + part->memtable_b.count;
+		stats->rows_cached += rows_cached;
+		stats->rows += rows_cached;
+
+		// size
+		if (part->index)
+		{
+			stats->size += part->index->size_total;
+			stats->size_uncompressed += part->index->size_total_origin;
+		}
+		uint64_t size_cached = part->memtable_a.size + part->memtable_b.size;
+		stats->size_cached += size_cached;
+		stats->size += size_cached;
+	}
+}
+
+hot static inline void
+storage_stats_print(Storage* self, Buf* buf)
+{
+	StorageStats stats;
+	storage_stats_init(&stats);
+	storage_stats(self, &stats);
+	buf_printf(buf, "%.*s\n", str_size(&self->target->name),
+	           str_of(&self->target->name));
+	buf_printf(buf, "  partitions        %" PRIu64 "\n", stats.partitions);
+	buf_printf(buf, "  min               %" PRIu64 "\n", stats.min);
+	buf_printf(buf, "  max               %" PRIu64 "\n", stats.max);
+	buf_printf(buf, "  rows              %" PRIu64 "\n", stats.rows);
+	buf_printf(buf, "  rows_cached       %" PRIu64 "\n", stats.rows_cached);
+	buf_printf(buf, "  size              %" PRIu64 " Mb\n", stats.size / 1024 / 1024);
+	buf_printf(buf, "  size_uncompressed %" PRIu64 " Mb\n", stats.size_uncompressed / 1024 / 1024);
+	buf_printf(buf, "  size_cached       %" PRIu64 " Mb\n", stats.size_cached / 1024 / 1024);
+}
