86b28caae5893e6960f54c572576d88ad1ba5c27
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-15 10:16:12 +0200

    engine: implement compaction logic

---
+++ monotone/engine/compaction.c
@@ -0,0 +1,170 @@
+
+//
+// monotone
+//
+// time-series storage
+//
+
+#include <monotone_runtime.h>
+#include <monotone_lib.h>
+#include <monotone_io.h>
+#include <monotone_storage.h>
+#include <monotone_engine.h>
+
+hot void
+compaction_execute(Compaction* self, ServicePart* ref, ServiceReq* req)
+{
+	auto engine = self->engine;
+
+	// take engine shared lock to prevent any exclusive ddl
+	lock_mgr_lock_shared(&engine->lock_mgr);
+	guard(lock_shard, lock_mgr_unlock_shared, &engine->lock_mgr);
+
+	// (1) find partition, match storage and rotate memtable
+	auto lock = engine_find(engine, false, ref->min);
+	assert(lock);
+	auto origin = lock->part;
+
+	// match storage, if provided by request
+	Storage* storage = NULL;
+	if (! str_empty(&req->storage))
+	{
+		storage = storage_mgr_find(&engine->storage_mgr, &req->storage);
+		if (unlikely(! storage))
+		{
+			lock_mgr_unlock(lock);
+			log("compaction: storage <%.*s> no longer available",
+			    str_size(&req->storage),
+			    str_of(&req->storage));
+			return;
+		}
+	}
+
+	// rotate memtable
+	auto memtable = part_memtable_rotate(origin);
+	lock_mgr_unlock(lock);
+
+	// get original partition storage, use it if the request storage was 
+	// not provided
+	auto origin_storage =
+		storage_mgr_find(&engine->storage_mgr, &origin->target->name);
+	if (storage == NULL)
+		storage = origin_storage;
+
+	// todo: target drop
+	
+	// (2) create new partition by merging existing partition file
+	//     with the memtable
+	MergerReq merger_req =
+	{
+		.origin   = origin,
+		.memtable = memtable,
+		.target   = storage->target
+	};
+	merger_reset(&self->merger);
+	merger_execute(&self->merger, &merger_req);
+	auto part = self->merger.part;
+
+	// (3) replace existing partition with the new one
+	lock = engine_find(engine, false, ref->min);
+	assert(lock);
+	assert(lock->part == origin);
+
+	// update partition tree and storages
+	mutex_lock(&engine->lock);
+
+	part_tree_replace(&engine->tree, origin, part);
+	storage_remove(origin_storage, origin);
+	storage_add(storage, part);
+
+	mutex_unlock(&engine->lock);
+
+	// reuse memtable
+	*part->memtable = *origin->memtable;
+	memtable_init(origin->memtable,
+	              origin->memtable->size_page,
+	              origin->memtable->size_split,
+	              origin->comparator);
+
+	lock_mgr_unlock(lock);
+
+	// (4) finilize and cleanup
+	
+	// free memtable memory
+	memtable_free(memtable);
+
+	// recovery crash case 1
+	//
+	// remove and free old partition
+	//
+	part_delete(origin, true);
+	part_free(origin);
+
+	// recovery crash case 2
+	//
+	// rename new partition
+	//
+	part_rename(part);
+}
+
+static void*
+compaction_main(void* arg)
+{
+	Compaction* self = arg;
+	runtime_init(self->context);
+	thread_set_name(&self->thread, "compaction");
+
+	for (;;)
+	{
+		ServicePart* part = NULL;
+		auto req = service_next(self->service, &part);
+		if (unlikely(req == NULL))
+			break;
+
+		Exception e;
+		if (try(&e))
+		{
+			compaction_execute(self, part, req);
+		}
+		if (catch(&e))
+		{ }
+
+		service_complete(self->service, part);
+	}
+
+	return NULL;
+}
+
+void
+compaction_init(Compaction* self)
+{
+	self->engine  = NULL;
+	self->service = NULL;
+	self->context = NULL;
+	merger_init(&self->merger);
+	thread_init(&self->thread);
+}
+
+void
+compaction_free(Compaction* self)
+{
+	merger_free(&self->merger);
+}
+
+void
+compaction_start(Compaction* self, Service* service, Engine* engine)
+{
+	self->service = service;
+	self->engine  = engine;
+	self->context = mn_runtime.context;
+	int rc;
+	rc = thread_create(&self->thread, compaction_main, self);
+	if (unlikely(rc == -1))
+		error_system();
+}
+
+void
+compaction_stop(Compaction* self)
+{
+	thread_join(&self->thread);
+}
---
+++ monotone/engine/compaction.h
@@ -0,0 +1,23 @@
+#pragma once
+
+//
+// monotone
+//
+// time-series storage
+//
+
+typedef struct Compaction Compaction;
+
+struct Compaction
+{
+	Merger   merger;
+	Engine*  engine;
+	Service* service;
+	Context* context;
+	Thread   thread;
+};
+
+void compaction_init(Compaction*);
+void compaction_free(Compaction*);
+void compaction_start(Compaction*, Service*, Engine*);
+void compaction_stop(Compaction*);
