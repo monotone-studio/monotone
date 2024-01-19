0dc5b0bbb960b9465fa470db0c8407d83f77d26d
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-19 15:00:04 +0200

    engine: use former catalog object as engine

--- monotone/engine/engine.c
+++ monotone/engine/engine.c
@@ -12,25 +12,36 @@
 #include <monotone_engine.h>
 
 void
-engine_init(Engine* self, Comparator* comparator)
+engine_init(Engine* self, Comparator* comparator, Service* service)
 {
+	self->service    = service;
 	self->comparator = comparator;
-	service_init(&self->service);
-	catalog_init(&self->catalog, comparator, &self->service);
+	rwlock_init(&self->lock_global);
+	mutex_init(&self->lock);
+	cond_var_init(&self->cond_var);
+	mapping_init(&self->mapping, comparator);
+	storage_mgr_init(&self->storage_mgr);
+	conveyor_init(&self->conveyor, &self->storage_mgr);
 }
 
 void
 engine_free(Engine* self)
 {
-	catalog_free(&self->catalog);
-	service_free(&self->service);
+	rwlock_free(&self->lock_global);
+	mutex_free(&self->lock);
+	cond_var_free(&self->cond_var);
+	conveyor_free(&self->conveyor);
+	storage_mgr_free(&self->storage_mgr);
 }
 
 void
 engine_open(Engine* self)
 {
-	// recover system objects
-	catalog_open(&self->catalog);
+	// recover storages
+	storage_mgr_open(&self->storage_mgr);
+
+	// recover conveyor
+	conveyor_open(&self->conveyor);
 
 	// recover partitions
 	engine_recover(self);
@@ -39,5 +50,119 @@ engine_open(Engine* self)
 void
 engine_close(Engine* self)
 {
-	catalog_close(&self->catalog);
+	storage_mgr_close(&self->storage_mgr);
+}
+
+void
+engine_lock_global(Engine* self, bool shared)
+{
+	rwlock_lock(&self->lock_global, shared);
+}
+
+void
+engine_unlock_global(Engine* self)
+{
+	rwlock_unlock(&self->lock_global);
+}
+
+static Slice*
+engine_create(Engine* self, uint64_t min, uint64_t max)
+{
+	// validate storage manager and conveyor
+	conveyor_validate(&self->conveyor);
+
+	auto head = mapping_max(&self->mapping);
+
+	// create new reference
+	auto ref  = ref_allocate(min, max);
+	guard(guard, ref_free, ref);
+
+	// create new partition
+	auto id   = config_psn_next();
+	auto part = part_allocate(self->comparator, NULL, id, id, min, max);
+	ref_prepare(ref, &self->lock, &self->cond_var, part);
+
+	// update mapping
+	mapping_add(&self->mapping, &ref->slice);
+
+	// match primary storage
+	Storage* storage = NULL;
+	Tier*    primary = conveyor_primary(&self->conveyor);
+	if (primary)
+	{
+		// first storage according to the conveyor order
+		storage = primary->storage;
+	} else
+	{
+		// conveyor is not set, using first storage
+		assert(self->storage_mgr.list_count == 1);
+		storage = storage_mgr_first(&self->storage_mgr);
+	}
+	storage_add(storage, part);
+	part->target = storage->target;
+
+	// schedule rebalance
+	if (! conveyor_empty(&self->conveyor))
+		service_rebalance(self->service);
+
+	// schedule refresh for previous head
+	if (head && head->min < min)
+		service_refresh(self->service, head->min);
+
+	return unguard(&guard);
+}
+
+hot Ref*
+engine_lock(Engine* self, uint64_t min, int lock,
+            bool gte,
+            bool create_if_not_exists)
+{
+	// take shared engine lock to avoid exclusive operations
+	engine_lock_global(self, true);
+	guard(cglguard, engine_unlock_global, self);
+
+	mutex_lock(&self->lock);
+	guard(unlock, mutex_unlock, &self->lock);
+
+	Slice* slice;
+	if (gte)
+	{
+		// engine is empty
+		if (unlikely(self->mapping.tree_count == 0))
+			return NULL;
+
+		// find partition >= min
+		slice = mapping_seek(&self->mapping, min);
+		if (slice->max <= min)
+			return NULL;
+	} else
+	{
+		// find partition = min
+		slice = mapping_match(&self->mapping, min);
+		if (! slice)
+		{
+			if (! create_if_not_exists)
+				return NULL;
+			auto max = min + config_interval();
+			slice = engine_create(self, min, max);
+		}
+	}
+
+	// take the reference locks
+	auto ref = ref_of(slice);
+	ref_lock(ref, lock);
+
+	// shared engine lock is kept until the reference
+	// is unlocked
+	unguard(&cglguard);
+	return ref;
+}
+
+void
+engine_unlock(Engine* self, Ref* ref, int lock)
+{
+	ref_unlock(ref, lock);
+
+	// unlock or dereference global engine lock
+	engine_unlock_global(self);
 }
--- monotone/engine/engine.h
+++ monotone/engine/engine.h
@@ -10,12 +10,25 @@ typedef struct Engine Engine;
 
 struct Engine
 {
-	Catalog     catalog;
-	Service     service;
+	// locking
+	Rwlock      lock_global;
+	Mutex       lock;
+	CondVar     cond_var;
+	// partition mapping
+	Mapping     mapping;
+	// objects
+	Conveyor    conveyor;
+	StorageMgr  storage_mgr;
+	Service*    service;
 	Comparator* comparator;
 };
 
-void engine_init(Engine*, Comparator*);
+void engine_init(Engine*, Comparator*, Service*);
 void engine_free(Engine*);
 void engine_open(Engine*);
 void engine_close(Engine*);
+
+void engine_lock_global(Engine*, bool);
+void engine_unlock_global(Engine*);
+Ref* engine_lock(Engine*, uint64_t, int, bool, bool);
+void engine_unlock(Engine*, Ref*, int);
