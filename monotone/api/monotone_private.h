c9cbce2873da376004b45d33dfa28cb7b2b8a3a3
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-19 16:04:19 +0200

    api: update includes

--- monotone/api/monotone_private.h
+++ monotone/api/monotone_private.h
@@ -9,7 +9,7 @@
 #include <monotone_runtime.h>
 #include <monotone_lib.h>
 #include <monotone_io.h>
-#include <monotone_storage.h>
+#include <monotone_catalog.h>
 #include <monotone_engine.h>
 #include <monotone_main.h>
 #include <monotone_api.h>
