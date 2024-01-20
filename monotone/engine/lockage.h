b4a4c7dc2fea2ce0d4b21e1aaff1f414920e193c
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-20 12:18:34 +0200

    engine: combine locks into lockage object

---
+++ monotone/engine/lockage.h
@@ -0,0 +1,104 @@
+#pragma once
+
+//
+// monotone
+//
+// time-series storage
+//
+
+typedef struct Lock    Lock;
+typedef struct Lockage Lockage;
+
+typedef enum
+{
+	LOCK_SERVICE,
+	LOCK_ACCESS,
+	LOCK_MAX
+} LockType;
+
+struct Lock
+{
+	pthread_t locker;
+	int       locker_refs;
+	int       pending;
+};
+
+struct Lockage
+{
+	Lock     locks[LOCK_MAX];
+	Mutex*   mutex;
+	CondVar* cond_var;
+};
+
+static inline void
+lockage_init(Lockage* self)
+{
+	LockType type = LOCK_SERVICE;
+	for (; type < LOCK_MAX; type++)
+	{
+		auto lock = &self->locks[type];
+		lock->locker      = 0;
+		lock->locker_refs = 0;
+		lock->pending     = 0;
+	}
+	self->mutex    = NULL;
+	self->cond_var = NULL;
+}
+
+static inline void
+lockage_set(Lockage* self, Mutex* mutex, CondVar* cond_var)
+{
+	self->mutex    = mutex;
+	self->cond_var = cond_var;
+}
+
+static inline void
+lockage_lock(Lockage* self, LockType type)
+{
+	// mutex must be always taken before this call
+	auto lock = &self->locks[type];
+
+	// nested call
+	pthread_t pthread_id = pthread_self();
+	if (pthread_equal(pthread_id, lock->locker))
+	{
+		lock->locker_refs++;
+		return;
+	}
+
+	// wait for lock
+	for (;;)
+	{
+		if (lock->locker == 0)
+		{
+			lock->locker = pthread_id;
+			lock->locker_refs = 0;
+			break;
+		}
+		lock->pending++;
+		cond_var_wait(self->cond_var, self->mutex);
+		lock->pending--;
+	}
+}
+
+static inline void
+lockage_unlock(Lockage* self, LockType type)
+{
+	// mutex must be always taken before this call
+	auto lock = &self->locks[type];
+
+	lock->locker_refs--;
+	if (lock->locker_refs > 0)
+		return;
+
+	// wakeup waiter
+	lock->locker = 0;
+	lock->locker_refs = 0;
+	if (lock->pending > 0)
+	{
+		// using broadcast since cond var can be shared between
+		// other lock types
+		cond_var_broadcast(self->cond_var);
+		return;
+	}
+}
--- monotone/engine/ref.h
+++ monotone/engine/ref.h
@@ -8,19 +8,12 @@
 
 typedef struct Ref Ref;
 
-typedef enum
-{
-	LOCK_SERVICE,
-	LOCK_ACCESS
-} RefLock;
-
 struct Ref
 {
-	bool  refresh;
-	Part* part;
-	Lock  lock_service;
-	Lock  lock_access;
-	Slice slice;
+	bool    refresh;
+	Part*   part;
+	Lockage lockage;
+	Slice   slice;
 };
 
 static inline Ref*
@@ -29,8 +22,7 @@ ref_allocate(uint64_t min, uint64_t max)
 	auto self = (Ref*)mn_malloc(sizeof(Ref));
 	self->refresh = false;
 	self->part    = NULL;
-	lock_init(&self->lock_service);
-	lock_init(&self->lock_access);
+	lockage_init(&self->lockage);
 	slice_init(&self->slice, min, max);
 	return self;
 }
@@ -47,8 +39,7 @@ static inline void
 ref_prepare(Ref* self, Mutex* mutex, CondVar* cond_var, Part* part)
 {
 	self->part = part;
-	lock_prepare(&self->lock_access, mutex, cond_var);
-	lock_prepare(&self->lock_service, mutex, cond_var);
+	lockage_set(&self->lockage, mutex, cond_var);
 }
 
 always_inline static inline Ref*
@@ -57,31 +48,14 @@ ref_of(Slice* slice)
 	return container_of(slice, Ref, slice);
 }
 
-static inline Lock*
-ref_lockof(Ref* self, RefLock lock)
+static inline void
+ref_lock(Ref* self, LockType type)
 {
-	Lock* ptr;
-	switch (lock) {
-	case LOCK_SERVICE:
-		ptr = &self->lock_service;
-		break;
-	case LOCK_ACCESS:
-		ptr = &self->lock_access;
-		break;
-	}
-	return ptr;
+	lockage_lock(&self->lockage, type);
 }
 
 static inline void
-ref_lock(Ref* self, RefLock type)
+ref_unlock(Ref* self, LockType type)
 {
-	auto lock = ref_lockof(self, type);
-	lock_lock(lock);
-}
-
-static inline void
-ref_unlock(Ref* self, RefLock type)
-{
-	auto lock = ref_lockof(self, type);
-	lock_unlock(lock);
+	lockage_unlock(&self->lockage, type);
 }
