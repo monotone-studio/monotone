ad9fac57016bd4cabbac43ee2d785b4c38164b3d
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-30 16:53:09 +0200

    command: add command parsing module

---
+++ monotone/main/command/monotone_command.h
@@ -0,0 +1,17 @@
+#pragma once
+
+//
+// monotone
+//
+// time-series storage
+//
+
+// command
+#include "command/cmd.h"
+
+// parse
+#include "command/parse_misc.h"
+#include "command/parse_config.h"
+#include "command/parse_storage.h"
+#include "command/parse_conveyor.h"
+#include "command/parse.h"
