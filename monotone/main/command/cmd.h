03b4ccaab213cb71dbc7de62d282cb244332d49b
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-30 16:53:22 +0200

    command: add cmd object

---
+++ monotone/main/command/cmd.h
@@ -0,0 +1,54 @@
+#pragma once
+
+//
+// monotone
+//
+// time-series storage
+//
+
+typedef struct Cmd Cmd;
+
+typedef void (*CmdFree)(Cmd*);
+
+typedef enum
+{
+	CMD_UNDEF,
+	CMD_SHOW,
+	CMD_SET,
+	CMD_CHECKPOINT,
+	CMD_STORAGE_CREATE,
+	CMD_STORAGE_DROP,
+	CMD_STORAGE_ALTER,
+	CMD_CONVEYOR_ALTER,
+	CMD_PARTITION_DROP,
+	CMD_PARTITION_DROP_RANGE,
+	CMD_PARTITION_MOVE,
+	CMD_PARTITION_MOVE_RANGE,
+	CMD_PARTITION_REFRESH,
+	CMD_PARTITION_REFRESH_RANGE,
+	CMD_REBALANCE
+} CmdType;
+
+struct Cmd
+{
+	CmdType type;
+	CmdFree free;
+};
+
+static inline void*
+cmd_allocate(CmdType type, CmdFree free, size_t size)
+{
+	assert(size >= sizeof(Cmd));
+	Cmd* self = mn_malloc(size);
+	self->type = type; 
+	self->free = free;
+	return self;
+}
+
+static inline void
+cmd_free(Cmd* self)
+{
+	if (self->free)
+		self->free(self);
+	mn_free(self);
+}
