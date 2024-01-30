2be0c67a50db942ab0374ef3eb05559efeb8e44a
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-30 13:58:17 +0200

    main: rename catalog module to storage

--- monotone/main/api/monotone_private.h
+++ monotone/main/api/monotone_private.h
@@ -10,7 +10,7 @@
 #include <monotone_lib.h>
 #include <monotone_config.h>
 #include <monotone_io.h>
-#include <monotone_catalog.h>
+#include <monotone_storage.h>
 #include <monotone_engine.h>
 #include <monotone_main.h>
 #include <monotone_api.h>
