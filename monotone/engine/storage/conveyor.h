863eec5d111ab074e514908e370c5819a7a23cdb
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-30 14:42:19 +0200

    storage: remove conveyor validate function

--- monotone/engine/storage/conveyor.h
+++ monotone/engine/storage/conveyor.h
@@ -23,14 +23,3 @@ void  conveyor_alter(Conveyor*, List*);
 void  conveyor_rename(Conveyor*, Str*, Str*);
 void  conveyor_print(Conveyor*, Buf*);
 Tier* conveyor_primary(Conveyor*);
-
-static inline void
-conveyor_validate(Conveyor* self)
-{
-	if (unlikely(self->storage_mgr->list_count == 0))
-		error("no storages created");
-
-	if (conveyor_empty(self) && self->storage_mgr->list_count > 1)
-		error("conveyor must be set, if there are more then "
-		      "one storage in the system");
-}
