c9f2e72da646939f39b0b8720e4a221662f1fd8a
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-30 14:12:58 +0200

    engine: move ddl functions out

--- monotone/engine/engine/op_ddl.h
+++ monotone/engine/engine/op_ddl.h
@@ -6,14 +6,3 @@
 // time-series storage
 //
 
-// storage
-void engine_storage_create(Engine*, Target*, bool);
-void engine_storage_drop(Engine*, Str*, bool);
-void engine_storage_alter(Engine*, Target*, int, bool);
-void engine_storage_rename(Engine*, Str*, Str*, bool);
-void engine_storage_show(Engine*, Str*, Buf*);
-void engine_storage_show_partitions(Engine*, Str*, Buf*);
-
-// conveyor
-void engine_conveyor_alter(Engine*, List*);
-void engine_conveyor_show(Engine*, Buf*);
