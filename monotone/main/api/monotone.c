123b490feb504b95038f8fb9ed1649d25305ba57
Dmitry Simonenko <pmwkaa@gmail.com>
2024-01-30 12:36:56 +0200

    main: add config module include

--- monotone/main/api/monotone.c
+++ monotone/main/api/monotone.c
@@ -5,13 +5,7 @@
 // time-series storage
 //
 
-#include <monotone_runtime.h>
-#include <monotone_lib.h>
-#include <monotone_io.h>
-#include <monotone_catalog.h>
-#include <monotone_engine.h>
-#include <monotone_main.h>
-#include <monotone_api.h>
+#include <monotone_private.h>
 #include <monotone.h>
 
 enum
--- monotone/main/api/monotone_private.h
+++ monotone/main/api/monotone_private.h
@@ -8,6 +8,7 @@
 
 #include <monotone_runtime.h>
 #include <monotone_lib.h>
+#include <monotone_config.h>
 #include <monotone_io.h>
 #include <monotone_catalog.h>
 #include <monotone_engine.h>
