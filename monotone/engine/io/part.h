88cccb3f7158876798e6946fe53b51448dc38413
Dmitry Simonenko <pmwkaa@gmail.com>
2024-02-03 10:56:43 +0200

    io: move alloc/free to part header

--- monotone/engine/io/part.h
+++ monotone/engine/io/part.h
@@ -10,41 +10,58 @@ typedef struct Part Part;
 
 struct Part
 {
+	Id          id;
 	bool        refresh;
 	bool        in_storage;
 	bool        in_cloud;
-	Id          id;
 	Memtable*   memtable;
 	Memtable    memtable_a;
 	Memtable    memtable_b;
 	File        file;
 	Index*      index;
 	Buf         index_buf;
+	Cloud*      cloud;
 	Source*     source;
 	Comparator* comparator;
 	List        link;
 };
 
-Part*
-part_allocate(Comparator*, Source*, Id*);
-void part_free(Part*);
+static inline Part*
+part_allocate(Comparator* comparator, Source* source, Id* id)
+{
+	auto self = (Part*)mn_malloc(sizeof(Part));
+	self->id         = *id;
+	self->refresh    = false;
+	self->in_storage = false;
+	self->in_cloud   = false;
+	self->index      = NULL;
+	self->cloud      = NULL;
+	self->source     = source;
+	self->comparator = comparator;
+	file_init(&self->file);
+	self->memtable = &self->memtable_a;
+	memtable_init(&self->memtable_a, 512, 508, comparator);
+	memtable_init(&self->memtable_b, 512, 508, comparator);
+	buf_init(&self->index_buf);
+	list_init(&self->link);
+	return self;
+}
 
-// partition file
-void part_open(Part*, bool);
-void part_create(Part*);
-void part_delete(Part*, bool);
-void part_rename(Part*);
+static inline void
+part_free(Part* self)
+{
+	memtable_free(&self->memtable_a);
+	memtable_free(&self->memtable_b);
+	file_close(&self->file);
+	buf_free(&self->index_buf);
+	mn_free(self);
+}
 
-// partition cloud file
-void part_cloud_open(Part*);
-void part_cloud_create(Part*);
-void part_cloud_delete(Part*, bool);
-void part_cloud_rename(Part*);
-
-// cloud
-void part_download(Part*, Cloud*);
-void part_upload(Part*, Cloud*);
-void part_offload(Part*, Cloud*, bool);
+static inline void
+part_set_cloud(Part* self, Cloud* cloud)
+{
+	self->cloud = cloud;
+}
 
 static inline Memtable*
 part_memtable_of(Part* self, Memtable** prev)
