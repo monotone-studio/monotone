952880123956b87070cc7527221d6c9bc064a43c
Dmitry Simonenko <pmwkaa@gmail.com>
2024-02-03 15:29:00 +0200

    engine: rework cloud functions, add if_cloud

--- monotone/engine/engine/op.c
+++ monotone/engine/engine/op.c
@@ -55,6 +55,10 @@ engine_move(Engine* self, Refresh* refresh, uint64_t min, Str* storage,
 void
 engine_drop(Engine* self, uint64_t min, bool if_exists)
 {
+	(void)self;
+	(void)min;
+	(void)if_exists;
+#if 0
 	// take exclusive control lock
 	control_lock_exclusive();
 
@@ -79,8 +83,9 @@ engine_drop(Engine* self, uint64_t min, bool if_exists)
 	control_unlock();
 
 	// delete partition file and free
-	part_delete(ref->part, true);
+	part_file_delete(ref->part, true);
 	ref_free(ref);
+#endif
 }
 
 void
@@ -221,7 +226,9 @@ engine_checkpoint(Engine* self)
 }
 
 void
-engine_download(Engine* self, uint64_t min, bool if_exists)
+engine_download(Engine* self, uint64_t min,
+                bool    if_exists,
+                bool    if_cloud)
 {
 	// find the original partition
 	auto ref = engine_lock(self, min, LOCK_SERVICE, false, false);
@@ -233,18 +240,16 @@ engine_download(Engine* self, uint64_t min, bool if_exists)
 	}
 	auto part = ref->part;
 
-	// get the original partition storage
-	auto storage = storage_mgr_find(&self->storage_mgr, &part->source->name);
-	assert(storage);
-
-	if (! storage->cloud)
+	if (! part->cloud)
 	{
 		engine_unlock(self, ref, LOCK_SERVICE);
-		error("download: partition <%" PRIu64 "> storage has no associated cloud", min);
+		if (! if_cloud)
+			error("download: partition <%" PRIu64 "> has no associated cloud", min);
+		return;
 	}
 
 	// partition file exists locally
-	if (part->in_storage)
+	if (part_has(part, PART_FILE))
 	{
 		engine_unlock(self, ref, LOCK_SERVICE);
 		return;
@@ -253,7 +258,7 @@ engine_download(Engine* self, uint64_t min, bool if_exists)
 	// download and open partition file
 	Exception e;
 	if (try(&e)) {
-		part_download(part, storage->cloud);
+		part_download(part);
 	}
 	if (catch(&e))
 	{
@@ -265,7 +270,7 @@ engine_download(Engine* self, uint64_t min, bool if_exists)
 	mutex_lock(&self->lock);
 	ref_lock(ref, LOCK_ACCESS);
 
-	part->in_storage = true;
+	part_set(part, PART_FILE);
 
 	ref_unlock(ref, LOCK_ACCESS);
 	mutex_unlock(&self->lock);
@@ -275,7 +280,9 @@ engine_download(Engine* self, uint64_t min, bool if_exists)
 }
 
 void
-engine_upload(Engine* self, uint64_t min, bool if_exists)
+engine_upload(Engine* self, uint64_t min,
+              bool    if_exists,
+              bool    if_cloud)
 {
 	// find the original partition
 	auto ref = engine_lock(self, min, LOCK_SERVICE, false, false);
@@ -287,27 +294,28 @@ engine_upload(Engine* self, uint64_t min, bool if_exists)
 	}
 	auto part = ref->part;
 
-	// get the original partition storage
-	auto storage = storage_mgr_find(&self->storage_mgr, &part->source->name);
-	assert(storage);
-
-	if (! storage->cloud)
+	if (! part->cloud)
 	{
 		engine_unlock(self, ref, LOCK_SERVICE);
-		error("upload: partition <%" PRIu64 "> storage has no associated cloud", min);
+		if (! if_cloud)
+			error("upload: partition <%" PRIu64 "> storage has no associated cloud", min);
+		return;
 	}
 
-	// partition already on cloud
-	if (part->in_cloud)
+	// partition already uploaded
+	if (part_has(part, PART_FILE_CLOUD))
 	{
 		engine_unlock(self, ref, LOCK_SERVICE);
 		return;
 	}
 
-	// download and open partition file
+	// todo:
+	assert(part_has(part, PART_FILE));
+
+	// create cloud file and upload data file
 	Exception e;
 	if (try(&e)) {
-		part_upload(part, storage->cloud);
+		part_upload(part);
 	}
 	if (catch(&e))
 	{
@@ -319,7 +327,7 @@ engine_upload(Engine* self, uint64_t min, bool if_exists)
 	mutex_lock(&self->lock);
 	ref_lock(ref, LOCK_ACCESS);
 
-	part->in_cloud = true;
+	part_set(part, PART_FILE_CLOUD);
 
 	ref_unlock(ref, LOCK_ACCESS);
 	mutex_unlock(&self->lock);
@@ -329,7 +337,9 @@ engine_upload(Engine* self, uint64_t min, bool if_exists)
 }
 
 void
-engine_offload(Engine* self, uint64_t min, bool from_storage, bool if_exists)
+engine_offload(Engine* self, uint64_t min, bool from_storage,
+               bool    if_exists,
+               bool    if_cloud)
 {
 	// find the original partition
 	auto ref = engine_lock(self, min, LOCK_SERVICE, false, false);
@@ -341,73 +351,68 @@ engine_offload(Engine* self, uint64_t min, bool from_storage, bool if_exists)
 	}
 	auto part = ref->part;
 
-	// get the original partition storage
-	auto storage = storage_mgr_find(&self->storage_mgr, &part->source->name);
-	assert(storage);
-
-	if (! storage->cloud)
+	if (! part->cloud)
 	{
 		engine_unlock(self, ref, LOCK_SERVICE);
-		error("offload: partition <%" PRIu64 "> storage has no associated cloud", min);
+		if (! if_cloud)
+			error("offload: partition <%" PRIu64 "> storage has no associated cloud", min);
+		return;
 	}
 
 	// remove from storage or cloud
 	if (from_storage)
 	{
 		// already removed
-		if (! part->in_storage)
+		if (! part_has(part, PART_FILE))
 		{
 			engine_unlock(self, ref, LOCK_SERVICE);
 			return;
 		}
 
-		// partition not in cloud
-		if (! part->in_cloud)
+		// partition not in the cloud
+		if (! part_has(part, PART_FILE_CLOUD))
 		{
 			engine_unlock(self, ref, LOCK_SERVICE);
-			error("offload: partition <%" PRIu64 "> must uploaded first", min);
+			error("offload: partition <%" PRIu64 "> must be uploaded first", min);
 		}
 
 	} else
 	{
 		// already removed
-		if (! part->in_cloud)
+		if (! part_has(part, PART_FILE_CLOUD))
 		{
 			engine_unlock(self, ref, LOCK_SERVICE);
 			return;
 		}
 
-		// partition not in storage
-		if (! part->in_storage)
+		// partition not in the storage
+		if (! part_has(part, PART_FILE))
 		{
 			engine_unlock(self, ref, LOCK_SERVICE);
 			error("offload: partition <%" PRIu64 "> must downloaded first", min);
 		}
 	}
 
-	// execute removal
-	Exception e;
-	if (try(&e)) {
-		part_offload(part, storage->cloud, from_storage);
-	}
-	if (catch(&e))
-	{
-		engine_unlock(self, ref, LOCK_SERVICE);
-		rethrow();
-	}
-
-	// update partition state
+	// update partition state first, to avoid heavy locking
 	mutex_lock(&self->lock);
 	ref_lock(ref, LOCK_ACCESS);
 
 	if (from_storage)
-		part->in_storage = false;
+		part_unset(part, PART_FILE);
 	else
-		part->in_cloud = false;
+		part_unset(part, PART_FILE_CLOUD);
 
 	ref_unlock(ref, LOCK_ACCESS);
 	mutex_unlock(&self->lock);
 
+	// execute removal
+	Exception e;
+	if (try(&e)) {
+		part_offload(part, from_storage);
+	}
+
 	// complete
 	engine_unlock(self, ref, LOCK_SERVICE);
+	if (catch(&e))
+		rethrow();
 }
--- monotone/engine/engine/op.h
+++ monotone/engine/engine/op.h
@@ -15,6 +15,6 @@ void engine_drop_range(Engine*, uint64_t, uint64_t);
 void engine_rebalance(Engine*, Refresh*);
 void engine_checkpoint(Engine*);
 
-void engine_download(Engine*, uint64_t, bool);
-void engine_upload(Engine*, uint64_t, bool);
-void engine_offload(Engine*, uint64_t, bool, bool);
+void engine_download(Engine*, uint64_t, bool, bool);
+void engine_upload(Engine*, uint64_t, bool, bool);
+void engine_offload(Engine*, uint64_t, bool, bool, bool);
