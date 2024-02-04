146bf93d1925a5ba4e7822d826f4eeb7e4c8d880
Dmitry Simonenko <pmwkaa@gmail.com>
2024-02-04 13:11:40 +0200

    storage: add cloud stats

--- monotone/engine/storage/storage_stats.h
+++ monotone/engine/storage/storage_stats.h
@@ -11,6 +11,7 @@ typedef struct StorageStats StorageStats;
 struct StorageStats
 {
 	uint64_t partitions;
+	uint64_t partitions_in_cloud;
 	uint64_t min;
 	uint64_t max;
 	uint64_t rows;
@@ -35,6 +36,9 @@ storage_stats(Storage* self, StorageStats* stats)
 	list_foreach(&self->list)
 	{
 		auto part = list_at(Part, link);
+
+		if (part_has(part, PART_FILE_CLOUD))
+			stats->partitions_in_cloud++;
 
 		// min/max
 		if (part->id.min < stats->min)
@@ -70,20 +74,24 @@ storage_stats_show(Storage* self, Buf* buf)
 	storage_stats(self, &stats);
 	buf_printf(buf, "%.*s\n", str_size(&source->name),
 	           str_of(&source->name));
-	buf_printf(buf, "  partitions        %" PRIu64 "\n", stats.partitions);
-	buf_printf(buf, "  min               %" PRIu64 "\n", stats.min);
-	buf_printf(buf, "  max               %" PRIu64 "\n", stats.max);
-	buf_printf(buf, "  rows              %" PRIu64 "\n", stats.rows);
-	buf_printf(buf, "  rows_cached       %" PRIu64 "\n", stats.rows_cached);
-	buf_printf(buf, "  size              %" PRIu64 " Mb\n", stats.size / 1024 / 1024);
-	buf_printf(buf, "  size_uncompressed %" PRIu64 " Mb\n", stats.size_uncompressed / 1024 / 1024);
-	buf_printf(buf, "  size_cached       %" PRIu64 " Mb\n", stats.size_cached / 1024 / 1024);
+	buf_printf(buf, "  min                 %" PRIu64 "\n", stats.min);
+	buf_printf(buf, "  max                 %" PRIu64 "\n", stats.max);
+	buf_printf(buf, "  partitions          %" PRIu64 "\n", stats.partitions);
+	buf_printf(buf, "  partitions in cloud %" PRIu64 "\n", stats.partitions_in_cloud);
+	buf_printf(buf, "  rows                %" PRIu64 "\n", stats.rows);
+	buf_printf(buf, "  rows_cached         %" PRIu64 "\n", stats.rows_cached);
+	buf_printf(buf, "  size                %" PRIu64 " Mb\n", stats.size / 1024 / 1024);
+	buf_printf(buf, "  size_uncompressed   %" PRIu64 " Mb\n", stats.size_uncompressed / 1024 / 1024);
+	buf_printf(buf, "  size_cached         %" PRIu64 " Mb\n", stats.size_cached / 1024 / 1024);
+
 	// source
-	buf_printf(buf, "  path              '%.*s'\n", str_size(&source->path),
+	buf_printf(buf, "  path                '%.*s'\n", str_size(&source->path),
 	           str_of(&source->path));
-	buf_printf(buf, "  sync              %s\n", source->sync ? "true" : "false");
-	buf_printf(buf, "  crc               %s\n", source->crc  ? "true" : "false");
-	buf_printf(buf, "  compression       %" PRIu64 "\n", source->compression);
-	buf_printf(buf, "  refresh_wm        %" PRIu64 "\n", source->refresh_wm);
-	buf_printf(buf, "  region_size       %" PRIu64 "\n", source->region_size);
+	buf_printf(buf, "  cloud               '%.*s'\n", str_size(&source->cloud),
+	           str_of(&source->cloud));
+	buf_printf(buf, "  sync                %s\n", source->sync ? "true" : "false");
+	buf_printf(buf, "  crc                 %s\n", source->crc  ? "true" : "false");
+	buf_printf(buf, "  compression         %" PRIu64 "\n", source->compression);
+	buf_printf(buf, "  refresh_wm          %" PRIu64 "\n", source->refresh_wm);
+	buf_printf(buf, "  region_size         %" PRIu64 "\n", source->region_size);
 }
