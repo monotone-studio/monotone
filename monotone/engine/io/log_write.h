35d6d687e608fd1447242cffb21eb648c1203f7d
Dmitry Simonenko <pmwkaa@gmail.com>
2024-02-12 16:21:48 +0200

    io: add log write object

---
+++ monotone/engine/io/log_write.h
@@ -0,0 +1,47 @@
+#pragma once
+
+//
+// monotone
+//
+// time-series storage
+//
+
+typedef struct LogWrite LogWrite;
+
+enum
+{
+	LOG_WRITE
+};
+
+struct LogWrite
+{
+	uint64_t lsn;
+	uint8_t  type;
+	uint32_t count;
+	uint32_t size;
+} packed;
+
+static inline void
+log_write_init(LogWrite* self)
+{
+	self->lsn   = 0;
+	self->type  = LOG_WRITE;
+	self->count = 0;
+	self->size  = 0;
+}
+
+static inline void
+log_write_reset(LogWrite* self)
+{
+	self->lsn   = 0;
+	self->type  = LOG_WRITE;
+	self->count = 0;
+	self->size  = 0;
+}
+
+static inline void
+log_write_add(LogWrite* self, Row* row)
+{
+	self->count++;
+	self->size += row_size(row);
+}
