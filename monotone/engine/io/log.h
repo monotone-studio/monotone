9e07e6c7545cd443ca3165e7679c64f3fad8e7cb
Dmitry Simonenko <pmwkaa@gmail.com>
2024-02-12 16:22:03 +0200

    io: combine wal write with log

--- monotone/engine/io/log.h
+++ monotone/engine/io/log.h
@@ -19,9 +19,24 @@ struct LogOp
 struct Log
 {
 	Buf      op;
-	int      op_count;
-	uint64_t lsn;
+	LogWrite write;
+	Iov      iov;
 };
+
+static inline void
+log_init(Log* self)
+{
+	buf_init(&self->op);
+	log_write_init(&self->write);
+	iov_init(&self->iov);
+}
+
+static inline void
+log_free(Log* self)
+{
+	buf_free(&self->op);
+	iov_free(&self->iov);
+}
 
 static inline LogOp*
 log_of(Log* self, int pos)
@@ -30,48 +45,57 @@ log_of(Log* self, int pos)
 	return &op[pos];
 }
 
-static inline void
-log_init(Log* self)
+static inline LogOp*
+log_begin(Log* self)
 {
-	self->op_count = 0;
-	self->lsn      = 0;
-	buf_init(&self->op);
+	return log_of(self, 0);
 }
 
-static inline void
-log_free(Log* self)
+static inline LogOp*
+log_end(Log* self)
 {
-	buf_free(&self->op);
+	return log_of(self, self->write.count);
 }
 
 static inline void
 log_cleanup(Log* self)
 {
-	for (int pos = 0; pos < self->op_count; pos++)
+	auto pos = log_begin(self);
+	auto end = log_end(self);
+	for (; pos < end; pos++)
 	{
-		auto op = log_of(self, pos);
-		if (op->row)
-			row_free(op->row);
+		if (pos->row)
+			row_free(pos->row);
 	}
 }
 
 static inline void
 log_reset(Log* self)
 {
-	self->op_count = 0;
-	self->lsn      = 0;
 	buf_reset(&self->op);
+	log_write_reset(&self->write);
+	iov_reset(&self->iov);
 }
 
 hot static inline LogOp*
 log_add(Log* self)
 {
+	// add log write header before the first entry
+	if (self->write.count == 0)
+		iov_add(&self->iov, &self->write, sizeof(self->write));
 	buf_reserve(&self->op, sizeof(LogOp));
 	auto op = (LogOp*)self->op.position;
 	op->row  = NULL;
 	op->prev = NULL;
 	op->ref  = NULL;
 	buf_advance(&self->op, sizeof(LogOp));
-	self->op_count++;
 	return op;
+}
+
+hot static inline void
+log_set(Log* self, LogOp* op, Row* row)
+{
+	op->row = row;
+	log_write_add(&self->write, row);
+	iov_add(&self->iov, row, row_size(row));
 }
