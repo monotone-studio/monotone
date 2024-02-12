6c12a42b3742e5ca3e4aeac1afc7528a7eea1977
Dmitry Simonenko <pmwkaa@gmail.com>
2024-02-12 16:23:13 +0200

    wal: add wal file object

---
+++ monotone/engine/wal/wal_file.h
@@ -0,0 +1,96 @@
+#pragma once
+
+//
+// monotone
+//
+// time-series storage
+//
+
+typedef struct WalFile WalFile;
+
+struct WalFile
+{
+	uint64_t id;
+	File     file;
+};
+
+static inline WalFile*
+wal_file_allocate(uint64_t id)
+{
+	WalFile* self = mn_malloc(sizeof(WalFile));
+	self->id = id;
+	file_init(&self->file);
+	return self;
+}
+
+static inline void
+wal_file_free(WalFile* self)
+{
+	mn_free(self);
+}
+
+static inline void
+wal_file_open(WalFile* self)
+{
+	char path[PATH_MAX];
+	snprintf(path, sizeof(path), "%s/wal/%020" PRIu64 ".wal",
+	         config_directory(),
+	         self->id);
+	file_open(&self->file, path);
+}
+
+static inline void
+wal_file_create(WalFile* self)
+{
+	char path[PATH_MAX];
+	snprintf(path, sizeof(path), "%s/wal/%020" PRIu64 ".wal",
+	         config_directory(),
+	         self->id);
+	file_create(&self->file, path);
+}
+
+static inline void
+wal_file_close(WalFile* self)
+{
+	file_close(&self->file);
+}
+
+static inline void
+wal_file_write(WalFile* self, struct iovec* iov, int iovc)
+{
+	file_writev(&self->file, iov, iovc);
+	if (var_int_of(&config()->wal_sync_on_write))
+		file_sync(&self->file);
+}
+
+static inline bool
+wal_file_eof(WalFile* self, uint32_t offset, uint32_t size)
+{
+	return (offset + size) > self->file.size;
+}
+
+static inline bool
+wal_file_pread(WalFile* self, uint64_t offset, Buf* buf)
+{
+	buf_reset(buf);
+
+	// check for eof
+	if (wal_file_eof(self, offset, sizeof(LogWrite)))
+		return true;
+
+	// read header
+	uint32_t size_header = sizeof(LogWrite);
+	int start = buf_size(buf);
+	file_pread_buf(&self->file, buf, size_header, offset);
+	uint32_t size = ((LogWrite*)(buf->start + start))->size;
+
+	// check for eof
+	if (wal_file_eof(self, offset, size))
+		return true;
+
+	// read body
+	uint32_t size_data;
+	size_data = size - size_header;
+	file_pread_buf(&self->file, buf, size_data, offset + size_header);
+	return false;
+}
