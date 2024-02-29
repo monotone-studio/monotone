99d3586c03d3e86e3f9af03ca67e66cfd227c848
Dmitry Simonenko <pmwkaa@gmail.com>
2024-02-29 15:47:16 +0200

    s3: separate io

--- monotone/cloud/s3/s3_io.h
+++ monotone/cloud/s3/s3_io.h
@@ -8,6 +8,8 @@
 
 typedef struct S3Io S3Io;
 
+typedef size_t (*S3Function)(void* ptr, size_t len, size_t nmemb, void* arg);
+
 struct S3Io
 {
 	void*  handle;
@@ -17,8 +19,3 @@ struct S3Io
 
 S3Io* s3_io_allocate(Cloud*);
 void  s3_io_free(S3Io*);
-void  s3_io_download(S3Io*, Source*, Id*, File*);
-void  s3_io_upload(S3Io*, Source*, Id*, File*);
-void  s3_io_delete(S3Io*, Source*, Id*);
-void  s3_io_read(S3Io*, Source*, Id*, Buf*, uint32_t, uint64_t);
-void  s3_io_create_bucket(S3Io*, Source*);
