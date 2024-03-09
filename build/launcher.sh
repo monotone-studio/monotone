63782b88f686c52e14325cf93c0bce0bbc1bffbe
Dmitry Simonenko <pmwkaa@gmail.com>
2024-03-09 17:40:11 +0200

    build: update launcher comments

--- build/launcher.sh
+++ build/launcher.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# match -c and -o
+# compiler call wrapper
+#
+# extract CC -c <path_compile> and -o <path_output>
+#
 path_compile=
 path_output=
 for ((arg=1; arg<=$#; arg++)); do
-    next=$((arg+1))
+	next=$((arg+1))
 	if [ "${!arg}" == "-c" ]; then
 		path_compile="${!next}"
 	elif [ "${!arg}" == "-o" ]; then
@@ -12,9 +15,11 @@ for ((arg=1; arg<=$#; arg++)); do
 	fi
 done
 
+# linker or compiler call
 if [ -z "$path_compile" ]; then
 	echo "$1 -o $path_output"
 else
+	# shorten path to <parent_dir>/<file>
 	path_rel=`echo ${path_compile} | awk -F "/" '{ print $(NF-1) "/" $NF }'`
 	echo "$1 -c ${path_rel}"
 fi
