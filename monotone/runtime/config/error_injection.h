6163d56285f9fa3e68bd42ab20b8b81c78970002
Dmitry Simonenko <pmwkaa@gmail.com>
2024-03-09 17:46:54 +0200

    update error injection output

--- monotone/runtime/config/error_injection.h
+++ monotone/runtime/config/error_injection.h
@@ -8,4 +8,4 @@
 
 #define error_injection(name) \
 	if (unlikely(var_int_of(&config()->name))) \
-		error("(%s:%d) error injection: %s", __FILE__, __LINE__, #name)
+		error("error injection: %s", #name)
